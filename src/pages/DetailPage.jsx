598c1d015285744bcb62f2c24ec3b9f0d016ef7b
mirgubad <Mirgubad@MIRGUBAD>
2024-05-02 12:37:45 +0400

    details fixed

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,79 +1,110 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import DetailsSlider from "../components/DetailsSlider";
-import DetailsTop from "../components/DetailsTop";
-import ProductDetails from "../components/ProductDetails";
-import SimilarItems from "../components/SimilarItems";
-import { useLang } from "../context/LangContext";
-import { useSetPageTitle } from "../hooks/useSetPageTitle";
-import { useGetProductByIdQuery } from "../services/product";
+import React, {useEffect, useState} from "react"
+import {useParams} from "react-router-dom"
+import DetailsSlider from "../components/DetailsSlider"
+import DetailsTop from "../components/DetailsTop"
+import ProductDetails from "../components/ProductDetails"
+import SimilarItems from "../components/SimilarItems"
+import {useLang} from "../context/LangContext"
+import {useSetPageTitle} from "../hooks/useSetPageTitle"
+import {useGetProductByIdQuery} from "../services/product"
 
 const DetailPage = () => {
-  const { lang } = useLang();
+  const {lang} = useLang()
   switch (lang) {
     case "Az":
-      useSetPageTitle("Məhsul");
-      break;
+      useSetPageTitle("Məhsul")
+      break
     case "Ru":
-      useSetPageTitle("Товар");
-      break;
+      useSetPageTitle("Товар")
+      break
     default:
-      useSetPageTitle("Product");
+      useSetPageTitle("Product")
   }
-  const params = useParams();
-  const { data: product, isLoading } = useGetProductByIdQuery(params.id);
+  const params = useParams()
+  // const {data: product, isLoading} = useGetProductByIdQuery(params.id)
+  const [product, setProduct] = useState(null)
 
+  const fetchProductById = async (id) => {
+    const url = "http://localhost:3000/products"
+
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const data = await response.json()
+      const filteredData = data.find((product) => product.id === +id)
+      setProduct(filteredData)
+
+      return filteredData
+    } catch (error) {
+      console.error("Failed to fetch popular products:", error)
+      throw error
+    }
+  }
+
+  useEffect(() => {
+    fetchProductById(params.id)
+  }, [params.id])
   return (
     <main>
-      <DetailsTop>
-        <DetailsSlider>
-          <img
-            style={{ width: "200px", height: "200px" }}
-            src={product?.img}
-            alt=""
-          />
-          <img
-            style={{ width: "200px", height: "200px" }}
-            src={product?.img}
-            alt=""
-          />
-          <img
-            style={{ width: "200px", height: "200px" }}
-            src={product?.img}
-            alt=""
-          />
-          <img
-            style={{ width: "200px", height: "200px" }}
-            src={product?.img}
-            alt=""
-          />
-          <img
-            style={{ width: "200px", height: "200px" }}
-            src={product?.img}
-            alt=""
-          />
-          <img
-            style={{ width: "200px", height: "200px" }}
-            src={product?.img}
-            alt=""
-          />
-          <img
-            style={{ width: "200px", height: "200px" }}
-            src={product?.img}
-            alt=""
-          />
-        </DetailsSlider>
-        {product && <ProductDetails lang={lang} {...product} />}
-      </DetailsTop>
       {product && (
-        <SimilarItems
-          lang={lang}
-          productId={product.id}
-          categoryId={product.categoryId}
-        />
+        <>
+          <DetailsTop>
+            <DetailsSlider>
+              <img
+                style={{width: "200px", height: "200px"}}
+                src={product?.img}
+                alt=""
+              />
+              <img
+                style={{width: "200px", height: "200px"}}
+                src={product?.img}
+                alt=""
+              />
+              <img
+                style={{width: "200px", height: "200px"}}
+                src={product?.img}
+                alt=""
+              />
+              <img
+                style={{width: "200px", height: "200px"}}
+                src={product?.img}
+                alt=""
+              />
+              <img
+                style={{width: "200px", height: "200px"}}
+                src={product?.img}
+                alt=""
+              />
+              <img
+                style={{width: "200px", height: "200px"}}
+                src={product?.img}
+                alt=""
+              />
+              <img
+                style={{width: "200px", height: "200px"}}
+                src={product?.img}
+                alt=""
+              />
+            </DetailsSlider>
+            {product && <ProductDetails lang={lang} {...product} />}
+          </DetailsTop>
+          <SimilarItems
+            lang={lang}
+            productId={product.id}
+            categoryId={product.categoryId}
+          />
+        </>
       )}
     </main>
-  );
-};
+  )
+}
 
-export default DetailPage;
+export default DetailPage
