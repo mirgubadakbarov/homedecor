0bd5eab2acc58454efffce34745d3a538c788702
mirgubad <Mirgubad@MIRGUBAD>
2024-05-02 10:38:15 +0400

    init

---
+++ dist/assets/index-b8cab669.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-43fb56a6.js";const e=({children:t})=>o.jsx("section",{className:"sectionTopBottom",children:o.jsx("div",{className:"sectionTopBottom__items",children:t})});export{e as S};
---
+++ dist/assets/useSetPageTitle-070a880b.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-43fb56a6.js";const r=e=>{t.useEffect(()=>{document.title=e},[e])};export{r as u};
